f4905e5302e741595b0f61f99e8c714996146b73
Igor Krasavin <krasavin.is@yandex.ru>
2019-10-08 19:47:19 +0300

    Init

---
+++ back/README.md
@@ -0,0 +1,10 @@
+# Summary
+back
+
+## Installation
+python init.py
+
+
+## Description 
+for Mac correct file init:
+system("venv\bin\activate")
---
+++ back/init.py
@@ -0,0 +1,6 @@
+from os import system
+
+
+system("python -m venv venv")
+system("venv\Scripts\activate")
+system("pip install -r requirements.txt")
